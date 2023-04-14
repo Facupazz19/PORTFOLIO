a83cfef6bedcb0b610c87c02ad0c8139aae78f1e
facupazz19 <faculapaz16@gmail.com>
2023-04-13 23:41:01 -0300

    solution-responsive

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const menuBtn = document.querySelector('.block');
-const navbar = document.querySelector('#nav-content');
-
-menuBtn.addEventListener('click', () => {
-    navbar.classList.toggle('hidden');
+// Esta es una función para abrir o cerrar el menú hamburguesa
+function toggleMobileMenu() {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+  }
+  
+// Agrega el evento de click al botón de hamburguesa
+  document.getElementById('mobile-menu-btn').addEventListener('click', function() {
+    toggleMobileMenu();
 });
+  
